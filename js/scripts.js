331e415147316cf103a41bf9692b09535032b913
kschneider726 <kschneider@vcd-ch204-01.eastern.ewu.edu>
2016-11-08 14:33:12 -0800

    commit .5

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
 // YOUR SCRIPTS
+
+//GRID VIEW 
+$('#grid').click( function(){
+    
+    //Remove ONLY 'list' class
+    $('.cd-gallery').removeClass('list');
+    //Add ONLY 'grid' class
+    $('.cd-gallery').addClass('grid');
+    
+    console.log('Go Vote!');
+    
+});
+
+//LIST VIEW 
+$('#list').click( function(){
+    
+    //Remove ONLY 'grid' class
+    $('.cd-gallery').removeClass('grid');
+    //Add ONLY 'list' class
+    $('.cd-gallery').addClass('list');
+    
+    console.log('Please!');
+    
+});
